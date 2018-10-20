c6381f74e9bf57f71cb649c3df3958fd51c025a5
obmeraz <44164215+obmeraz@users.noreply.github.com>
2018-10-21 00:08:25 +0300

    ch

--- Practical task 2/task1/DragonLogic.java
+++ Practical task 2/task1/DragonLogic.java
@@ -4,20 +4,19 @@ public class DragonLogic {
     public static final int TWO_HUNDRED_YEARS = 200;
     public static final int THREE_HUNDRED_YEARS = 300;
 
-    public static final int TWO_HUNDRED_YEARS_HEADS = 600;
-    public static final int ONE_HUNDRED_YEARS_HEADS = 200;
+    public static final int START_HEADS = 3;
 
-    public static final int EYES_NUMBER = 200;
+    public static final int EYES_NUMBER = 2;
 
     public static int calculateHeads(int age) {
         if (age < TWO_HUNDRED_YEARS) {
-            return age * 3;
+            return age * 3 + START_HEADS;
         } else if (age > TWO_HUNDRED_YEARS && age < THREE_HUNDRED_YEARS) {
             age -= TWO_HUNDRED_YEARS;
-            return age * 2 + TWO_HUNDRED_YEARS_HEADS;
+            return age * 2 + 600 + START_HEADS;
         } else {
             age -= THREE_HUNDRED_YEARS;
-            return age + TWO_HUNDRED_YEARS_HEADS + ONE_HUNDRED_YEARS_HEADS;
+            return age + 600 + 200 + START_HEADS;
         }
     }
 
